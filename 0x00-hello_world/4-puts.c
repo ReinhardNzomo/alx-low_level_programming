8d6c4fc685efdc738fb075dba9239c24ba39af44
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-08 07:22:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  *Description: main prints Programming is .......
  * Return 0 if successful.
- * */
-int main(void)
+ * 
+ */
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
