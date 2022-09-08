416fee1f0a7570bb7b711c8ee1469cc27fbeeb40
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-08 08:37:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - print and that.....
  * Return: return 1 if successful
  */
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
