fb61975a71c53f35f38896bd1e5e6466028bf596
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-09 12:21:33 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lower, caps followed by newline
+ *
+ * Return: 0 always!!!!
+ */
+int main(void)
+{
+	char ch;
+	char CH;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for(CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
+	putcchar('\n');
+
+	return (0);
+}
