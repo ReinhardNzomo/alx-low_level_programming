3a75cbdb80fb0836b0890d2be92883708c3f74d4
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-12 04:49:01 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - function
+* print_alphabet print the lowercase alphs
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
