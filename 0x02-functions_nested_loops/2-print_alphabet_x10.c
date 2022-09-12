930f15b20a8496d4176aca048d0fc474869df9c7
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-12 04:59:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+* print_alphabet_x10 -> prints the lowercase 10x alphabets
+*/
+void print_alphabet_x10(void)
+{
+	int a;
+	int ch;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
