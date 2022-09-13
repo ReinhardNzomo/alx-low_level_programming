75ae6eb4d26a226f85194fff676556e79bc99708
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-12 23:40:17 -0700

    hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,27 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = '0'; j < '10'; j++)
+		for (j = 0; j < 10; j++)
 		{
 			int x;
 
 			x = (i * j);
-			_putchar(x + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+
+			if (x < 10)
+			{
+				_putchar(x + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(x +'0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('$');
 		_putchar('\n');
