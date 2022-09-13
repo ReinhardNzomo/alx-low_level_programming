7cf48a415bdbf5cce598e120a4f72aac236acd3f
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-12 23:00:31 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* times_table -> print multiple table
+* Return: table
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		for (j = '0'; j < '10'; j++)
+		{
+			int x;
+
+			x = (i * j);
+			_putchar(x + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
