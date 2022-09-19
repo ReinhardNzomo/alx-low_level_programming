ee436ac8719705470312c300aa583d412e8b9789
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-19 08:18:00 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s - i))
+	while (*(s + i))
+		i++;
+
+	i = i - 1;
+	while (i >= 0)
 	{
 		_putchar(*(s - i));
-		i++;
+		i--;
 	}
 	_putchar('\n');
 }
