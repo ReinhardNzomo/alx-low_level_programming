937c1614089e942a5f5074fe49be6ec12c9c2685
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-19 16:20:05 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,77 +1,31 @@
-// C program for the above approach
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-  
-// Function to randomly generates password
-// of length N
-void randomPasswordGeneration(int N)
-{
-    // Initialize counter
-    int i = 0;
-  
-    int randomizer = 0;
-  
-    // Seed the random-number generator
-    // with current time so that the
-    // numbers will be different every time
-    srand((unsigned int)(time(NULL)));
-  
-    // Array of numbers
-    char numbers[] = "0123456789";
-  
-    // Array of small alphabets
-    char letter[] = "abcdefghijklmnoqprstuvwyzx";
-  
-    // Array of capital alphabets
-    char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-  
-    // Array of all the special symbols
-    char symbols[] = "!@#$^&*?";
-  
-    // Stores the random password
-    char password[N];
-  
-    // To select the randomizer
-    // inside the loop
-    randomizer = rand() % 4;
-  
-    // Iterate over the range [0, N]
-    for (i = 0; i < N; i++) {
-  
-        if (randomizer == 1) {
-            password[i] = numbers[rand() % 10];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 2) {
-            password[i] = symbols[rand() % 8];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else if (randomizer == 3) {
-            password[i] = LETTER[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-        else {
-            password[i] = letter[rand() % 26];
-            randomizer = rand() % 4;
-            printf("%c", password[i]);
-        }
-    }
-}
-  
-// Driver Code
+/**
+* main - keygen
+* Return: 0
+*/
+
 int main()
 {
-    // Length of the password to
-    // be generated
-    int N = 10;
-  
-    // Function Call
-    randomPasswordGeneration(N);
-  
-    return 0;
+	int i, password, len;
+
+	len = 10;
+	srand((unsigned int) time(NULL));
+	for (i = 0; i < len; i++)
+	{
+		int k = rand() % 128;
+
+		if ((k >= 48 && k<= 57) || (k >= 65 && k <= 90) || (k >= 97 && k<= 122) || (k >= 35 && k <= 37) || k == 64)
+		{
+			printf("%c", k);
+		}
+		else
+		{
+			i--;
+		}
+	}
+	printf("\n");
+
+	return (0);
 }
