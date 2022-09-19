1071623789d7ff3aeb40b8f6f20953bffb5b230f
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-19 08:14:08 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_rev- print str in reverse
+* @s: string
+* Return: 0
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s - i))
+	{
+		_putchar(*(s - i));
+		i++;
+	}
+	_putchar('\n');
+}
