7af9324b189cb612904cc8f3ffae204c8fb7c3be
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-21 03:16:36 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* *_strncpy- copy string src to dest
+* @dest: param1
+* @src: param2
+* @n: param integer
+* Return: string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, char, n);
+	return (dest);
+}
