b01a27780fb495d9309c9527e0c2c1089995e9a7
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-26 07:06:14 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _strspn- gets the length of a prefix substring.
+* @s: source string
+* @accept: accepted string
+* Return: number of bytes
+*/
+
+unsigned int (char *s, char *accept)
+{
+	int a = 0, b, t = 0;
+
+	while (accept[a])
+	{
+		b = 0;
+
+		while (s[b])
+		{
+			if (s[b] != 32)
+			{
+				t++;
+			}
+			b++
+		}
+		a++;
+	}
+
+	return (t);
+}
