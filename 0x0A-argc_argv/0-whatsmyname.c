4e504bf072a55aeabaf496b3399a3dfb7762f2d5
ReinhardNzomo <nzomo.mr@gmail.com>
2022-09-30 13:32:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main- print own name
+* @argc: count
+* @argv: command line strings
+* Return: )
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
