1e5512e24ea5cbc56a5b3135e8f6a9885f21caec
ReinhardNzomo <nzomo.mr@gmail.com>
2022-10-06 07:12:27 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* malloc_checked - allocates memory using malloc
+* @b: size of mem block to be allocated
+* Return: pointer to address of mem block
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem_block;
+
+	mem_block = malloc(b);
+
+	if (mem_block == NULL)
+		exit(98);
+	
+	return (mem_block);
+}
