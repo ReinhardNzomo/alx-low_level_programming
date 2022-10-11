4a399d13907862e30f24cbe5a629309a278a3824
ReinhardNzomo <nzomo.mr@gmail.com>
2022-10-11 15:28:37 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <string.h>
 /**
 * new_dog - new dog
 * @name: dog name
@@ -16,9 +16,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	d->name = name;
+	d->name = strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = strcpy(d->owner, owner);
 
 	return (d);
 }
