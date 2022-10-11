c35c5a8b9c45b8b71519960c21e1c20a6ce70c7d
ReinhardNzomo <nzomo.mr@gmail.com>
2022-10-11 16:24:20 -0700

    4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,37 +1,59 @@
 #include "dog.h"
+#include <stdlib.h>
+
 /**
- * new_dog - new cute dog.
-*
-*@name: name of dog.
-*@age: age of dog.
-*@owner: owner of dog.
-*
-* Return: Always 0.
-*/
+ * _strcopy - copy read only data to mutatable.
+ * @dst: pointer to copy char to.
+ * @src: read only data.
+ */
+void _strcopy(char *dst, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dst[i] = src[i];
+	dst[i] = '\0';
+}
+
+/**
+ * new_dog - create new dog from the dna of the first dog.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *x;
+	dog_t *d;
+	int a, b;
 
-	x = malloc(sizeof(dog_t));
+	for (a = 0; name[a]; a++)
+		;
+	for (b = 0; owner[b]; b++)
+		;
 
-	if (x == NULL)
+	d = malloc(sizeof(dog_t));
+	if (!d)
 		return (NULL);
 
-	if (name == NULL)
+	d->name = malloc(a + 1);
+	if (!d->name)
 	{
-		free(x);
-		free(owner);
+		free(d);
 		return (NULL);
 	}
-	if (owner == NULL)			
+
+	d->owner = malloc(b + 1);
+	if (!d->owner)
 	{
-		free(x);
-		free(name);
+		free(d->name);
+		free(d);
 		return (NULL);
 	}
-	x->name = name;
-	x->age = age;
-	x->owner = owner;
 
-	return (x);
+	_strcopy(d->name, name);
+	_strcopy(d->owner, owner);
+	d->age = age;
+	return (d);
 }
