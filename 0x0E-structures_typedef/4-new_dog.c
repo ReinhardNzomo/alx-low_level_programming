0af868dab0a08a8bc620149327c08f14ceab4e90
ReinhardNzomo <nzomo.mr@gmail.com>
2022-10-11 15:35:13 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,24 +1,80 @@
 #include "dog.h"
-#include <string.h>
-/**
-* new_dog - new dog
-* @name: dog name
-* @age: age of dog
-* @owner: owner of dog
-* Return: dog
-*/
 
+
+/**
+*_strlen - returns length of
+*a string
+*@str: string to be counted
+*Return: returns length of string
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (str)
+len++;
+
+return (len);
+}
+
+
+/**
+*_strcpy - copy string pointed by src
+*into dest variable
+*@dest:buffer storing string copy
+*@src: buffer storing string to copy
+*Return:returns copied string
+*/
+char *_strcpy(char *dest, char *src)
+{
+int index = 0;
+
+for (; src[index] ; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
+
+
+
+
+/**
+*new_dog - creates a new dog
+*@name: name of new dog
+*@age: age of new dog
+*@owner: owner of new dog
+*Return: returns NULL in case
+*of failure
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
+dog_t *d;
 
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
+if (name == NULL || age < 0 || owner == NULL)
+return (NULL);
 
-	d->name = strcpy(d->name, name);
-	d->age = age;
-	d->owner = strcpy(d->owner, owner);
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+return (NULL);
 
-	return (d);
+d->name = malloc(sizeof(char) * (_strlen(name) + 1));
+if (d->name == NULL)
+{
+free(d);
+return (NULL);
+}
+
+d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+if (d->owner == NULL)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+
+d->name = _strcpy(d->name, name);
+d->age = age;
+d->owner = _strcpy(d->owner, owner);
+
+return (d);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,4 +24,6 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
 #endif
