28dd2d1e4ca4a8186be7324e223dd74505f45cfd
ReinhardNzomo <nzomo.mr@gmail.com>
2022-10-11 15:55:34 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,80 +1,27 @@
 #include "dog.h"
-
-
+#include <stdio.h>
+#include <string.h>
 /**
-*_strlen - returns length of
-*a string
-*@str: string to be counted
-*Return: returns length of string
+ * new_dog - new cute dog.
+*
+*@name: name of dog.
+*@age: age of dog.
+*@owner: owner of dog.
+*
+* Return: Always 0.
 */
-int _strlen(char *str)
-{
-int len = 0;
-while (str)
-len++;
+	dog_t *new_dog(char *name, float age, char *owner)
+	{
+	dog_t *x;
 
-return (len);
-}
+	x = malloc(sizeof(dog_t));
 
+	if (x == NULL)
+	return (NULL);
 
-/**
-*_strcpy - copy string pointed by src
-*into dest variable
-*@dest:buffer storing string copy
-*@src: buffer storing string to copy
-*Return:returns copied string
-*/
-char *_strcpy(char *dest, char *src)
-{
-int index = 0;
+	x->name = strcpy(x->name, name);
+	x->age = age;
+	x->owner = strcpy(x->owner, owner);
 
-for (; src[index] ; index++)
-dest[index] = src[index];
-
-dest[index] = '\0';
-return (dest);
-}
-
-
-
-
-/**
-*new_dog - creates a new dog
-*@name: name of new dog
-*@age: age of new dog
-*@owner: owner of new dog
-*Return: returns NULL in case
-*of failure
-*/
-dog_t *new_dog(char *name, float age, char *owner)
-{
-dog_t *d;
-
-if (name == NULL || age < 0 || owner == NULL)
-return (NULL);
-
-d = malloc(sizeof(dog_t));
-if (d == NULL)
-return (NULL);
-
-d->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (d->name == NULL)
-{
-free(d);
-return (NULL);
-}
-
-d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (d->owner == NULL)
-{
-free(d->name);
-free(d);
-return (NULL);
-}
-
-d->name = _strcpy(d->name, name);
-d->age = age;
-d->owner = _strcpy(d->owner, owner);
-
-return (d);
+	return (x);
 }
