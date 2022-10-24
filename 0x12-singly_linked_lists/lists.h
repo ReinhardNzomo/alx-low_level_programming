904a0d58dff5021a48d928b79633390fa6895729
ReinhardNzomo <nzomo.mr@gmail.com>
2022-10-24 00:30:45 -0700

    adding lists header file

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+* struct list_s - singly linked list structure
+* @str: string
+* @len: string length
+* @next: pointer to next node
+*
+* Description: structure of a singly linked list
+*/
+
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
