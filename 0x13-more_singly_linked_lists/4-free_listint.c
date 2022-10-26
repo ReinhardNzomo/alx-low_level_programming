3b5c8cfdae6db554666a4642da4b235a14afe1a9
ReinhardNzomo <nzomo.mr@gmail.com>
2022-10-26 12:20:29 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,27 +1,17 @@
-#include "lists.h"
-
 /**
-* free_listint2 - free linked list
+* free_listint - free linked list
 * @head: pointer to head of the list
 * Return: void
 */
 
-void free_listint2(listint_t **head)
+void free_listint(listint_t *head)
 {
-	listint_t *ptr;
-	listint_t **temp_node = head;
+	listint_t *temp_node;
 
-	if (temp_node != NULL)
+	while (head)
 	{
-		while (*head != NULL)
-		{
-			ptr = *head;
-			free(ptr);
-			*head = (*head)->next;
-		}
-	}
-	else
-	{
-		*temp_node = NULL;
+		temp_node = head;
+		head = head->next;
+		free(temp_node);
 	}
 }
