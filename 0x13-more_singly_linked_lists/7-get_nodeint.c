85ca15d9cd2fb3ab20a36dc5b65f4c960fe25117
ReinhardNzomo <nzomo.mr@gmail.com>
2022-10-26 15:02:10 -0700

    get nth node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - get nth node of list
+* @head: pointer to the lis
+* @index: n
+* Return: nth node of list
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int counter;
+	listint_t temp_node;
+
+	counter = 0;
+
+	while (head)
+	{
+		temp_node = head;
+
+		if (counter == index)
+		{
+			return (temp_node);
+		}
+		head = head->next;
+		counter++;
+	}
+	
+	return (NULL);
+}
